03e579d369352d43f0449f33f0856ce77f05de77
fangyequan <fangyequan15@mails.ucas.ac.cn>
2017-05-21 16:47:31 +0800

    first committ

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=answer',
+    'username' => 'root',
+    'password' => 'psw.db7898',
+    'charset' => 'utf8',
+];
