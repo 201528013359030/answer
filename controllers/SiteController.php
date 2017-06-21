aaf8a19d0b25fdc084cd71e6cb7d8ddef2e1388e
fangyequan <fangyequan15@mails.ucas.ac.cn>
2017-06-21 16:43:37 +0800

    solve yii2 Unable to verify your data submission

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -10,6 +10,8 @@ use app\models\LoginForm;
 use app\models\ContactForm;
 
 class SiteController extends Controller {
+	public $enableCsrfValidation = false;
+
 	public $servername = "localhost";
 	public $username = "root";
 	public $password = "psw.db7898";
@@ -22,6 +24,50 @@ class SiteController extends Controller {
 	 */
 // 	function __construct() {
 // 	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors()
+	{
+		return [
+				'access' => [
+						'class' => AccessControl::className(),
+						'only' => ['logout'],
+						'rules' => [
+								[
+										'actions' => ['logout'],
+										'allow' => true,
+										'roles' => ['@'],
+								],
+						],
+				],
+				'verbs' => [
+						'class' => VerbFilter::className(),
+						'actions' => [
+								'logout' => ['post'],
+						],
+				],
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function actions()
+	{
+		return [
+				'error' => [
+						'class' => 'yii\web\ErrorAction',
+				],
+				'captcha' => [
+						'class' => 'yii\captcha\CaptchaAction',
+						'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+				],
+		];
+	}
+
+
 	/**
 	 * Displays homepage.
 	 *
